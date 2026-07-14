8e08c1b0a404d1e156361d207cd8056dbf4bd0a7
Ehab Salah <e.salah82055@gmail.com>
2026-07-15 00:38:11 +0300

    Feat: Add JsonHelper, NetworkConstants and SecretConstants Files
    - JsonHelper help us to show the json response with good way for us
    - NetworkConstants and SecretConstants to add the network constants in it

---
+++ Medsy/core/networking/SecretConstants.swift
@@ -0,0 +1,14 @@
+//
+//  SeacretConstants.swift
+//  shopify-ecommerce-ios
+//
+//  Created by albaraa alsayed on 16/01/1448 AH.
+//
+
+import Foundation
+
+struct SecretConstants {
+    static let apiKey = ""
+    static let hostname = ""
+    static let password = ""
+}
