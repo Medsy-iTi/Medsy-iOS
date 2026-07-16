e28fc82719ee70647c47b6faa55e52c709781d80
Ehab Salah <e.salah82055@gmail.com>
2026-07-16 04:10:44 +0300

    Wire app flows through coordinator

--- Medsy/MedsyApp.swift
+++ Medsy/MedsyApp.swift
@@ -13,6 +13,7 @@ struct MedsyApp: App {
     private let languageManager: LanguageManager
     private let onboardingFactory: OnboardingFactory
     private let authenticationFactory: AuthenticationFactory
+    private let appCoordinator: AppCoordinator
 
     init() {
         AppAssembler.shared.assemble(modules: [
@@ -24,13 +25,15 @@ struct MedsyApp: App {
         languageManager = AppAssembler.shared.container.resolve(LanguageManager.self)
         onboardingFactory = AppAssembler.shared.container.resolve(OnboardingFactory.self)
         authenticationFactory = AppAssembler.shared.container.resolve(AuthenticationFactory.self)
+        appCoordinator = AppCoordinator(shouldShowOnboarding: onboardingFactory.shouldShow())
     }
 
     var body: some Scene {
         WindowGroup {
             AppRootView(
                 onboardingFactory: onboardingFactory,
-                authenticationFactory: authenticationFactory
+                authenticationFactory: authenticationFactory,
+                coordinator: appCoordinator
             )
                 .localizedEnvironment()
                 .environment(languageManager)
