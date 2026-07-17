ec29ec2188add6f07bae6bc15656616bfce66b33
ahmedelkady757 <ahmed.elkady757@gmail.com>
2026-07-17 17:45:47 +0300

    Register pharmacy authentication dependencies

--- Medsy-Pharmacy/Features/Authentication/DI/PharmacyAuthenticationAssembly.swift
+++ Medsy-Pharmacy/Features/Authentication/DI/PharmacyAuthenticationAssembly.swift
@@ -7,6 +7,31 @@
 
 struct PharmacyAuthenticationAssembly: PharmacyModuleAssembly {
     func register(in container: PharmacyDIContainer) {
+        container.register(PharmacyAuthenticationRemoteDataSourceProtocol.self) { container in
+            PharmacyAuthenticationRemoteDataSource(
+                networkService: container.resolve(NetworkServiceProtocol.self)
+            )
+        }
+
+        container.register(PharmacyAuthenticationRepositoryProtocol.self) { container in
+            PharmacyAuthenticationRepository(
+                remoteDataSource: container.resolve(PharmacyAuthenticationRemoteDataSourceProtocol.self)
+            )
+        }
+
+        container.register(PharmacyRegistrationUseCaseProtocol.self) { container in
+            PharmacyRegistrationUseCase(
+                repository: container.resolve(PharmacyAuthenticationRepositoryProtocol.self)
+            )
+        }
+
+        container.register(PharmacyVerificationUseCaseProtocol.self) { container in
+            PharmacyVerificationUseCase(
+                repository: container.resolve(PharmacyAuthenticationRepositoryProtocol.self),
+                tokenStore: container.resolve(TokenStoreProtocol.self)
+            )
+        }
+
         container.register(PharmacyAuthenticationActions.self) { _ in
             .placeholder
         }
