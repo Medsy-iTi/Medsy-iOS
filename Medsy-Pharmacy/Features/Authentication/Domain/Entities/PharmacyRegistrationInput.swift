58f78ecd96b24e443012a6f8fd63855fe8b3cca6
ahmedelkady757 <ahmed.elkady757@gmail.com>
2026-07-17 17:02:27 +0300

    Add pharmacy authentication domain entities

---
+++ Medsy-Pharmacy/Features/Authentication/Domain/Entities/PharmacyRegistrationInput.swift
@@ -0,0 +1,14 @@
+//
+//  PharmacyRegistrationInput.swift
+//  Medsy-Pharmacy
+//
+//  Created by Ahmed Elkady on 17/07/2026.
+//
+
+struct PharmacyRegistrationInput: Equatable {
+    let firstName: String
+    let lastName: String
+    let phoneNumber: String
+    let email: String
+    let password: String
+}
