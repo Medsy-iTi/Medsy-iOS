8a86fe361612e77457d650fb12f2249d8df2a101
ahmedelkady757 <ahmed.elkady757@gmail.com>
2026-07-17 16:31:32 +0300

    Add pharmacy secrets configuration

--- SharedCore/Networking/NetworkRequestBuilder.swift
+++ SharedCore/Networking/NetworkRequestBuilder.swift
@@ -10,13 +10,18 @@ import Foundation
 
 final class NetworkRequestBuilder {
     private let languageManager: LanguageManager
+    private let defaultBaseURL: String
 
-    init(languageManager: LanguageManager) {
+    init(
+        languageManager: LanguageManager,
+        defaultBaseURL: String = Constants.baseURL
+    ) {
         self.languageManager = languageManager
+        self.defaultBaseURL = defaultBaseURL
     }
 
     func makeRequest(for endpoint: ApiEndpoint, accessToken: String? = nil) throws -> URLRequest {
-        let urlString = (endpoint.baseURL ?? Constants.baseURL) + endpoint.path
+        let urlString = (endpoint.baseURL ?? defaultBaseURL) + endpoint.path
 
         guard let url = URL(string: urlString) else {
             throw NetworkError.invalidURL
