d842be6e7c77050ce08be511da6771b8928d342a
antoneosphilip <109253442+antoneosphilip@users.noreply.github.com>
2026-07-20 19:41:40 -0700

    feat(pharmacy-profile): add PharmacyMapper for data transformation

---
+++ Medsy/Features/PharmacyProfile/Data/Mappers/PharmacyMapper.swift
@@ -0,0 +1,19 @@
+//  PharmacyMapper.swift
+//  Medsy
+//
+//  Created by Antoneos Philip on 20/07/2026.
+
+import Foundation
+
+enum PharmacyMapper {
+    static func map(_ dto: PharmacyDataDTO) -> Pharmacy {
+        Pharmacy(
+            id: dto.id,
+            name: dto.name,
+            latitude: dto.latitude,
+            longitude: dto.longitude,
+            address: dto.address,
+            phoneNumber: dto.phoneNumber
+        )
+    }
+}
