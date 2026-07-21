ef02ea490014f5e084579982f82bd83bab78eadb
antoneosphilip <109253442+antoneosphilip@users.noreply.github.com>
2026-07-20 19:41:06 -0700

    feat(pharmacy-profile): add Pharmacy domain entity

---
+++ Medsy/Features/PharmacyProfile/Domain/Entities/Pharmacy.swift
@@ -0,0 +1,20 @@
+//  Pharmacy.swift
+//  Medsy
+//
+//  Created by Antoneos Philip on 20/07/2026.
+
+import Foundation
+import CoreLocation
+
+struct Pharmacy: Identifiable, Hashable, Sendable {
+    let id: Int
+    let name: String
+    let latitude: Double
+    let longitude: Double
+    let address: String
+    let phoneNumber: String
+
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
