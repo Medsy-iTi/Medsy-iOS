abb7b79c6486579a271d6a35c9a09e2fc7714370
shahd <shahdashraf396@gmail.com>
2026-07-21 20:34:19 -0700

    add repo

---
+++ Medsy-Pharmacy/Features/Orders/Data/Repository/PharmacyOrdersRepository.swift
@@ -0,0 +1,29 @@
+//
+//  PharmacyOrdersRepository.swift
+//  Medsy
+//
+//  Created by Shahudaa on 21/07/2026.
+//
+
+
+
+final class PharmacyOrdersRepository: PharmacyOrdersRepositoryProtocol {
+    private let networkService: NetworkServiceProtocol
+
+    init(networkService: NetworkServiceProtocol) {
+        self.networkService = networkService
+    }
+
+    func fetchOrders(pharmacyId: Int, page: Int, size: Int) async throws -> PharmacyOrdersPage {
+        let endpoint = PharmacyOrdersEndpoint.fetchOrders(pharmacyId: pharmacyId, page: page, size: size)
+
+        let envelope: APIEnvelope<PageResponseDTO<PharmacyOrderDTO>> =
+            try await networkService.request(endpoint: endpoint)
+
+        guard let pageDTO = envelope.data else {
+            throw NetworkError.decodingFailed
+        }
+
+        return PharmacyOrderMapper.map(pageDTO)
+    }
+}
