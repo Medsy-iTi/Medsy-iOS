15e4f85c78089bed162f38e7c880d0e68104c07b
antoneosphilip <109253442+antoneosphilip@users.noreply.github.com>
2026-07-22 15:12:20 -0700

    make repository protocol

---
+++ Medsy-Pharmacy/Features/Request Details/Domain/Repository/PharmacyRequestDetailsRepositoryProtocol.swift
@@ -0,0 +1,12 @@
+//  PharmacyRequestDetailsRepositoryProtocol.swift
+//  Medsy-Pharmacy
+//
+//  Created by Antoneos Philip on 23/07/2026.
+//
+
+import Foundation
+
+protocol PharmacyRequestDetailsRepositoryProtocol {
+    func fetchRequestDetails(requestId: Int) async throws -> PharmacyRequestDetailsEntity
+    func fetchPharmacyRequests(pharmacyId: Int, page: Int, size: Int) async throws -> [PharmacyRequestDetailsEntity]
+}
