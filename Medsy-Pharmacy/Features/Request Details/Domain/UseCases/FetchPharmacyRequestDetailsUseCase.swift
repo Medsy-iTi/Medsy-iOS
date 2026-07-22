16705f563892358e7230cb82e3c97402826ed4d7
antoneosphilip <109253442+antoneosphilip@users.noreply.github.com>
2026-07-22 15:12:32 -0700

    make usecase

---
+++ Medsy-Pharmacy/Features/Request Details/Domain/UseCases/FetchPharmacyRequestDetailsUseCase.swift
@@ -0,0 +1,24 @@
+//  FetchPharmacyRequestDetailsUseCase.swift
+//  Medsy-Pharmacy
+//
+//  Created by Antoneos Philip on 23/07/2026.
+//
+
+import Foundation
+
+protocol FetchPharmacyRequestDetailsUseCaseProtocol {
+    func execute(requestId: Int) async throws -> PharmacyRequestDetailsEntity
+    func execute(pharmacyId: Int, page: Int, size: Int) async throws -> [PharmacyRequestDetailsEntity]
+}
+
+struct FetchPharmacyRequestDetailsUseCase: FetchPharmacyRequestDetailsUseCaseProtocol {
+    let repository: PharmacyRequestDetailsRepositoryProtocol
+
+    func execute(requestId: Int) async throws -> PharmacyRequestDetailsEntity {
+        try await repository.fetchRequestDetails(requestId: requestId)
+    }
+
+    func execute(pharmacyId: Int, page: Int, size: Int) async throws -> [PharmacyRequestDetailsEntity] {
+        try await repository.fetchPharmacyRequests(pharmacyId: pharmacyId, page: page, size: size)
+    }
+}
