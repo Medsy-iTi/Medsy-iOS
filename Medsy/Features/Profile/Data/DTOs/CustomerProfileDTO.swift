bebabce0a14c4fc8b95cd8b2900fa36b5a7be9c0
shahd <shahdashraf396@gmail.com>
2026-07-22 20:39:10 -0700

    change dto to be ready for add lat , long of address

--- Medsy/Features/Profile/Data/DTOs/CustomerProfileDTO.swift
+++ Medsy/Features/Profile/Data/DTOs/CustomerProfileDTO.swift
@@ -10,32 +10,42 @@ import Foundation
 typealias CustomerProfileResponseDTO = APIResponseDTO<CustomerProfileDTO>
 
 struct CustomerProfileDTO: Decodable, Equatable {
-    let id: Int
-    let email: String
-    let firstName: String
-    let lastName: String
-    let homeAddress: String?
-    let dob: String?
-    let phoneNumber: String
+	let id: Int
+	let email: String
+	let firstName: String
+	let lastName: String
+	let homeAddress: String?
+	let latitude: Double?
+	let longitude: Double?
+	let dob: String?
+	let phoneNumber: String
 }
 
 struct UpdateCustomerProfileRequestDTO: Encodable, Equatable {
     let homeAddress: String?
+	let latitude: Double?
+	let longitude: Double?
     let dob: String?
 
     init(input: UpdateCustomerProfileInput) {
         homeAddress = input.homeAddress
+		latitude = input.homeLatitude
+		longitude = input.homeLongitude
         dob = input.dateOfBirth.map(ProfileDateMapper.string)
     }
 
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encodeIfPresent(homeAddress, forKey: .homeAddress)
+		try container.encodeIfPresent(latitude, forKey: .latitude)
+		try container.encodeIfPresent(longitude, forKey: .longitude)
         try container.encodeIfPresent(dob, forKey: .dob)
     }
 
     private enum CodingKeys: String, CodingKey {
         case homeAddress
+		case latitude
+		case longitude
         case dob
     }
 }
--- Medsy/Features/Profile/Data/Mappers/CustomerProfileMapper.swift
+++ Medsy/Features/Profile/Data/Mappers/CustomerProfileMapper.swift
@@ -14,6 +14,8 @@ enum CustomerProfileMapper {
             lastName: dto.lastName,
             homeAddress: dto.homeAddress,
             dateOfBirth: dto.dob.flatMap(ProfileDateMapper.date),
+			homeLatitude: dto.latitude,
+			homeLongitude: dto.longitude,
             phoneNumber: dto.phoneNumber
         )
     }
