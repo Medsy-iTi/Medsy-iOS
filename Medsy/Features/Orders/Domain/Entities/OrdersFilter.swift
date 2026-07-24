a7f96b06c1a38274df5549ae415e6fe53a42b127
Ahmed Elkady <ahmed.elkady757@gmail.com>
2026-07-24 20:43:12 +0300

    fix(orders/domain): add manual Equatable conformance for OrdersFilter due to OrderStatus associated value

--- Medsy/Features/Orders/Domain/Entities/OrdersFilter.swift
+++ Medsy/Features/Orders/Domain/Entities/OrdersFilter.swift
@@ -7,11 +7,22 @@
 
 import Foundation
 
-struct OrdersFilter: Equatable {
+struct OrdersFilter {
     var statuses: [OrderStatus]?
     var fulfillmentType: OrderFulfillmentType?
     var dateFrom: Date?
     var dateTo: Date?
 
     static let empty = OrdersFilter()
+}
+
+extension OrdersFilter: Equatable {
+    static func == (lhs: OrdersFilter, rhs: OrdersFilter) -> Bool {
+        let lhsStatuses = lhs.statuses?.map(\.rawValue)
+        let rhsStatuses = rhs.statuses?.map(\.rawValue)
+        return lhsStatuses == rhsStatuses
+            && lhs.fulfillmentType == rhs.fulfillmentType
+            && lhs.dateFrom == rhs.dateFrom
+            && lhs.dateTo == rhs.dateTo
+    }
 }
