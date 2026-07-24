3a93a3e1652544fff4acee3576702cda6fc82543
Ahmed Elkady <ahmed.elkady757@gmail.com>
2026-07-24 20:43:53 +0300

    feat(orders/presentation): add ActiveOrderFilters, OrderDateRangeFilter, and domain filter conversion

---
+++ Medsy/Features/Orders/Presentation/Models/ActiveOrderFilters.swift
@@ -0,0 +1,31 @@
+//
+//  ActiveOrderFilters.swift
+//  Medsy
+//
+//  Created by Ahmed Elkady on 24/07/2026.
+//
+
+import Foundation
+
+struct ActiveOrderFilters: Equatable {
+    var statusFilter: OrderFilter = .all
+    var dateRangeFilter: OrderDateRangeFilter = .anytime
+    var customDateFrom: Date? = nil
+    var customDateTo: Date? = nil
+    var fulfillmentType: OrderFulfillmentType? = nil
+
+    static let `default` = ActiveOrderFilters()
+
+    var hasActiveFilters: Bool {
+        statusFilter != .all
+            || dateRangeFilter != .anytime
+            || fulfillmentType != nil
+    }
+
+    func resolvedDateRange() -> (from: Date?, to: Date?) {
+        if dateRangeFilter == .custom {
+            return (customDateFrom, customDateTo)
+        }
+        return dateRangeFilter.dateRange()
+    }
+}
