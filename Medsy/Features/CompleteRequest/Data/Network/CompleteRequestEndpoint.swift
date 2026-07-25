a359923499d506c8a631d73b4f9d738c1995393a
Ehab Salah <e.salah82055@gmail.com>
2026-07-25 20:54:33 +0300

    feat(complete-request): update request contract

--- Medsy/Features/CompleteRequest/Data/Network/CompleteRequestEndpoint.swift
+++ Medsy/Features/CompleteRequest/Data/Network/CompleteRequestEndpoint.swift
@@ -19,14 +19,48 @@ enum CompleteRequestEndpoint: ApiEndpoint {
         .post
     }
 
+    var headers: HTTPHeaders? {
+        ["Accept": "application/json"]
+    }
+
     var body: Data? {
+        nil
+    }
+
+    var multipartFormParts: [MultipartFormPart]? {
         switch self {
         case let .submit(request):
-            try? JSONEncoder().encode(request)
+            guard let requestData = try? JSONEncoder().encode(request) else {
+                return nil
+            }
+
+            var parts = [
+                MultipartFormPart(
+                    data: requestData,
+                    name: "request",
+                    mimeType: "application/json"
+                )
+            ]
+
+            if let prescriptionData = request.prescriptionData {
+                parts.append(
+                    MultipartFormPart(
+                        data: prescriptionData,
+                        name: "prescription",
+                        fileName: "prescription.jpg",
+                        mimeType: "image/jpeg"
+                    )
+                )
+            }
+            return parts
         }
     }
 
     var requiresAuthentication: Bool {
+        true
+    }
+
+    var allowsResponseLogging: Bool {
         true
     }
 }
